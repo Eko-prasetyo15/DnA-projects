9f42a29d14db99f3a4b0173f24a1c2bf5ccdf94f
Eko-Prasetyo15 <prasetyoeko822@gmail.com>
2020-04-29 23:34:02 +0700

    add feature

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,11 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'DNA photgraphy' });
 });
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'DNA photgraphy' });
+});
 
+router.get('/portfolio', function(req, res, next) {
+  res.render('portfolio', { title: 'DNA photgraphy' });
+});
 module.exports = router;
